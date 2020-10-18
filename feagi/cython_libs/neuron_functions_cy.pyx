6798d3b6641b71592aa11f7204a1278e986786e2
Eleven11Wang <janewang4332@gmail.com>
2020-10-18 20:32:21 +0800

    ..

--- feagi/cython_libs/neuron_functions_cy.pyx
+++ feagi/cython_libs/neuron_functions_cy.pyx
@@ -1,14 +1,9 @@
-# Copyright (c) 2019 Mohammad Nadji-Tehrani <m.nadji.tehrani@gmail.com>
 def neuron_update(float presynaptic_current,
                   int burst_count,
                   int last_membrane_potential_update,
                   float leak_coefficient,
                   float membrane_potential):
-    """
-    This function's only task is to update the membrane potential of the destination neuron.
-    """
 
-    # Leaky behavior
     if leak_coefficient > 0:
         if last_membrane_potential_update < burst_count:
             leak_window = burst_count - last_membrane_potential_update
@@ -17,8 +12,6 @@ def neuron_update(float presynaptic_current,
             if membrane_potential < 0:
                 membrane_potential = 0
 
-    # Increasing the cumulative counter on destination based on the received signal from upstream Axon
-    # The following is considered as LTP or Long Term Potentiation of Neurons
 
     membrane_potential += presynaptic_current
 
